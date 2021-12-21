20c1ed2994038075f290c6fb5c09121816127363
eun-chae-s <s9710173@naver.com>
2021-12-21 00:13:24 -0500

    improve design

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import './App.css';
 
 function App() {
-  return <div>Let's create a weather app!</div>;
+  return (
+    <div>
+      <div className="header">
+        <h1>ONEUL</h1>
+      </div>
+      <div className="search">
+        <input type="text" placeholder="Location..?"></input>
+        <button type="submit">&#x1F50D;</button>
+      </div>
+      <div className="weather">
+        Hello
+      </div>
+    </div>
+  
+  );
 }
 
 export default App;
