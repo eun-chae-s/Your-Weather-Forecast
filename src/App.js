e704cf2a0e1dd1ce91d48eeb980de1a060ecbaa7
eun-chae-s <s9710173@naver.com>
2021-12-22 16:53:47 -0500

    Mostly finished the design of the app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import './App.css';
+import rainy from './img/rainy.png';
+import cloudSunny from './img/sun.png';
+import snow from './img/snowing.png';
+
+// TODO: make a separate page for the credit
 
 function App() {
   return (
@@ -10,9 +15,35 @@ function App() {
         <input type="text" placeholder="Location..?"></input>
         <button type="submit">&#x1F50D;</button>
       </div>
+      <h2>Location</h2>
       <div className="weather">
-        Hello
+        
+        <div id="today">
+          <h2>Today</h2>
+          <img src={ rainy } alt="rainy"></img>
+        </div>
+        <div className="not-today">
+          <div id="tmr">
+            <h2>Tomorrow</h2>
+            <img src={ cloudSunny } alt="Cloudy and Sunny"></img>
+          </div>
+          <div id="day-after-tmr">
+            <h2>Friday</h2>
+            <img src={ snow } alt="Snow"></img>
+          </div>
+          <div id="extra">
+            <h2>Feedback?</h2>
+          </div>
+        </div>
+        
       </div>
+      {/* Make a separate credit page that displays all the references */}
+      <footer>
+        <button id="credit" type="button">Credit</button>
+        {/* <div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div> */}
+        {/* <div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div> */}
+        {/* <div>Icons made by <a href="https://www.flaticon.com/authors/winnievinzence" title="winnievinzence">winnievinzence</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div> */}
+      </footer>
     </div>
   
   );
